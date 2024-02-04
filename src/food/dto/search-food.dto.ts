7980d8dd78666a260dfe2da4933dfe0c9ea06d70
wintery39 <wintery39@naver.com>
2024-02-04 16:05:17 +0900

    recommend

--- src/food/dto/search-food.dto.ts
+++ src/food/dto/search-food.dto.ts
@@ -3,37 +3,44 @@ import { IsNotEmpty } from "class-validator";
 
 export class searchFoodDto{
     @ApiProperty({
-        example: 0,
+        example: 200,
         description: '에너지',
     })
     @IsNotEmpty()
     energy_kcal: number;
 
     @ApiProperty({
-        example: 0,
+        example: 200,
         description: '수분',
     })
     @IsNotEmpty()
     water_g!: number;
 
     @ApiProperty({
-        example: 0,
+        example: 200,
         description: '단백질',
     })
     @IsNotEmpty()
     protein_g: number;
 
     @ApiProperty({
-        example: 0,
+        example: 200,
         description: '지방',
     })
     @IsNotEmpty()
     fat_g: number;
 
     @ApiProperty({
-        example: 0,
+        example: 200,
         description: '탄수화물',
     })
     @IsNotEmpty()
     carbohydrate_g: number;
+
+    @ApiProperty({
+        example: 1,
+        description: '부족영양소',
+    })
+    @IsNotEmpty()
+    lack_nutrient: number;
 }
--- src/food/food.repository.ts
+++ src/food/food.repository.ts
@@ -1,4 +1,4 @@
-import { LessThan, Like, MoreThan, Repository } from 'typeorm';
+import { Between, LessThan, Like, MoreThan, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
 import { FOOD } from './entities/food.entity';
@@ -21,6 +21,19 @@ export class FoodRepository extends Repository<FOOD> {
   }
 
   async findBySearch(search: searchFoodDto) {
-    return await this.repository.find({where: [({energy_kcal: LessThan(search.energy_kcal)}) && ({water_g: LessThan(search.water_g)})]});
+    switch (search.lack_nutrient) {
+      case 1:
+        return await this.repository.find({where: [{energy_kcal: Between(search.energy_kcal*(9/11),search.energy_kcal), water_g: LessThan(search.water_g), protein_g: LessThan(search.protein_g), fat_g: LessThan(search.fat_g), carbohydrate_g: LessThan(search.carbohydrate_g)}]});
+      case 2:
+        return await this.repository.find({where: [{energy_kcal: LessThan(search.energy_kcal), water_g: Between(search.water_g*(9/11),search.water_g), protein_g: LessThan(search.protein_g), fat_g: LessThan(search.fat_g), carbohydrate_g: LessThan(search.carbohydrate_g)}]});
+      case 3:
+        return await this.repository.find({where: [{energy_kcal: LessThan(search.energy_kcal), water_g: LessThan(search.water_g), protein_g: Between(search.protein_g*(9/11),search.protein_g), fat_g: LessThan(search.fat_g), carbohydrate_g: LessThan(search.carbohydrate_g)}]});
+      case 4:
+        return await this.repository.find({where: [{energy_kcal: LessThan(search.energy_kcal), water_g: LessThan(search.water_g), protein_g: LessThan(search.protein_g), fat_g: Between(search.fat_g*(9/11), search.fat_g), carbohydrate_g: LessThan(search.carbohydrate_g)}]});
+      case 5:  
+        return await this.repository.find({where: [{energy_kcal: LessThan(search.energy_kcal), water_g: LessThan(search.water_g), protein_g: LessThan(search.protein_g), fat_g: LessThan(search.fat_g), carbohydrate_g: Between(search.carbohydrate_g*(9/11), search.carbohydrate_g)}]});
+      default:
+        return 'input error';
+    }
   }
 }
